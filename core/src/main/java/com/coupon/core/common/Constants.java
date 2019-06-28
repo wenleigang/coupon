f6fa987792defcc25ff3b74d88f6ee729dd857fe
Rayn.Gosling <wenleigang0926@163.com>
2019-06-28 15:35:56 +0800

    调整返利比率

--- core/src/main/java/com/coupon/core/common/Constants.java
+++ core/src/main/java/com/coupon/core/common/Constants.java
@@ -13,14 +13,66 @@ package com.coupon.core.common;
  * @Version: 1.0
  */
 public interface Constants {
+    //===================== 请求相关参数定义 ===================================================================
+    String CONTEXT_PATH = "ctx"; //当前项目根地址
+    String BACK_URL = "backURL"; //请求的上一次的地址
+    String CURRENT_FIELDS = "fields";
+    String NO_QUERYSTRING_CURRENT_URL = "noQueryStringCurrentURL";
 
-    /*********************************** 图灵机器人 *************************************/
+    //===================== 时间类型参数定义参数定义 =============================================================
+    String YMD_HMS = "yyyy-MM-dd HH:mm:ss";
+    String YMD_HM = "yyyy-MM-dd HH:mm";
+    String YMD = "yyyy-MM-dd";
+    String YM = "yyyy-MM";
+    String YMD_HMS_CN = "yyyy年MM月dd日 HH时:mm分:ss秒";
+    String YMD_HM_CN = "yyyy年MM月dd日 HH时:mm分";
+    String YMD_CN = "yyyy年MM月dd日";
+    String YM_CN = "yyyy年MM月";
+
+    //===================== 服务端接口数据校验正则表达式 ==========================================================
+    /**
+     * 手机号
+     */
+    String REGEX_PHONE = "^((17[0-9])|(14[0-9])|(13[0-9])|(15[^4,\\D])|(18[0-9]))\\d{8}$";
+
+    /**
+     * 数字和字母
+     */
+    String REGEX_NUM_LATTER = "^[a-zA-Z0-9]*$";
+
+    /**
+     * 数字和字母(6-20)
+     */
+    String REGEX_PASSWORD = "^[a-zA-Z0-9]{6,20}$";
+
+    /**
+     * 汉字
+     */
+    String REGEX_CHINESE = "^[\u4e00-\u9fa5]*$";
+
+    /**
+     * 身份证号
+     */
+    String REGEX_IDENTITY_CARD_NUM = "^(\\d{6})(18|19|20)?(\\d{2})([01]\\d)([0123]\\d)(\\d{3})(\\d|X|x)?$";
+
+    /**
+     * 非0正整数
+     */
+    String REGEX_POSITIVE_INTEGER = "^[1-9]\\d*$";
+
+    /**
+     * 正数
+     */
+    String REGEX_POSITIVE_DOUBLE = "^[+]?[.\\d]*$";
+
+
+    //===================== 图灵机器人 ============================================================================
     //图灵机器人接口调用地址
     String TULING_URL = "http://www.tuling123.com/openapi/api";
     //图灵机器人apikey
     String TULING_APIKEY = "42f52b0939ae4a159da5bdcd9988036e";
 
-    /*********************************** 淘宝/喵有劵 *************************************/
+    //===================== 淘宝/喵有劵 ============================================================================
     //喵有劵对接秘钥
     String MYJ_APKEY = "177982ef-f5bc-0b00-b2e9-2430763ffd67";
 
@@ -30,7 +82,7 @@ public interface Constants {
     //在喵有劵授权登录后的淘宝名称
     String TBNAME = "wenleigang834147811";
 
-    /*********************************** 淘宝客api详细接口 *************************************/
+    //===================== 淘宝客api详细接口 ========================================================================
     //淘宝客API请求地址
     String TBK_URL = "https://api.open.21ds.cn/apiv1";
 
@@ -43,7 +95,7 @@ public interface Constants {
     //万能高佣转链API接口
     String GY_ALL = TBK_URL+"/getgyurlbyall?apkey="+MYJ_APKEY+"&pid="+PID+"&tbname="+TBNAME+"&shorturl=1&tpwd=1&content=";
 
-    /*********************************** 其他格式需要接口查询优惠券 *************************************/
+    //===================== 其他格式需要接口查询优惠券 ===================================================================
     //（1）产品链接 //item.taobao.com/item.htm?id=578327349261
     String URL_TYPE_ITEM_LINK = "item.taobao.com";
     String URL_TYPE_TMALL_ITEM_LINK = "detail.tmall.com";
--- front/src/main/webapp/WEB-INF/view/jsp/hello.jsp
+++
@@ -1,5 +0,0 @@
-<html>
-<body>
-<h2>Hello jsp World!</h2>
-</body>
-</html>
